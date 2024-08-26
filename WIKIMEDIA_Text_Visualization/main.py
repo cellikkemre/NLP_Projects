dc26e5becf4454e159b5957ee1498c102ed5e74e
Emre Çelik <cellikkemre@gmail.com>
2024-08-26 12:26:13 +0300

    .

--- WIKIMEDIA_Text_Visualization/main.py
+++ WIKIMEDIA_Text_Visualization/main.py
@@ -2,7 +2,7 @@ from data_loader import load_data
 from text_pipeline import text_pipeline
 
 # Load the dataset
-df = load_data('data/your_dataset.csv', n_rows=2000)
+df = load_data('WIKIMEDIA_Text_Visualization/wiki_data.csv', n_rows=2000)
 
 # Run the text processing pipeline
 processed_text = text_pipeline.fit_transform(df['text_column'])
